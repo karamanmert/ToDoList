4fd40981e6cb5e9f091705541316e21ff0db56b2
unknown <mertkrmn06@gmail.com>
2019-10-19 13:40:29 +0300

    first commit

---
+++ Task1/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task1.Models;
+
+
+namespace Task1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public static List<item> items = new List<item>();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            return View(items);
+        }
+
+
+        [HttpGet]
+        public ActionResult Yeni()
+        {
+
+
+            return View("Form");
+        }
+
+        [HttpPost]
+        public ActionResult Kaydet(item itm)
+        {
+            
+            if(itm.id==0)
+            {
+                items.Add(new item(itm.not,itm.tarih));
+            }
+            else
+            {
+                var guncellenecek = items.FirstOrDefault(m => m.id == itm.id);
+                guncellenecek.not = itm.not;
+                guncellenecek.tarih = itm.tarih;
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Guncelle(int id)
+        {
+            var model = items.FirstOrDefault(m => m.id == id);
+            return View("Form",model);
+        }
+        public ActionResult Sil(int id)
+        {
+            var silinecek = items.FirstOrDefault(m => m.id == id);
+            items.Remove(silinecek);
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ Task1/Models/item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace Task1.Models
+{
+    public class item
+    {
+        public static int no = 0;
+        public int id { get; set; }
+        public String not { get; set; }
+        public String tarih {get;set;}
+        public item(String not,String tarih)
+        {
+            this.id = ++no;
+            this.not = not;
+            this.tarih = tarih;
+
+        }
+        public item()
+        {
+
+        }
+    }
+}
